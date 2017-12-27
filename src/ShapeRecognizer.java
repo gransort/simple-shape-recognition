c2c7b351cf981835a2be0c16c3b0e4ea62cec600
Vyacheslav Pisarevskiy <pisarevsc@yandex.ru>
2017-12-27 19:43:14 +0400

    Final implementation the figure search algorithm
    - For searching shapes on picture now used Ramer-Douglas-Peucker
    algorithm

--- src/ShapeRecognizer.java
+++ src/ShapeRecognizer.java
@@ -1,13 +1,11 @@
 import org.opencv.core.*;
-import org.opencv.imgproc.Imgproc;
-
-import static org.opencv.imgproc.Imgproc.*;
-
 import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.opencv.imgproc.Imgproc.COLOR_RGB2GRAY;
 
 public class ShapeRecognizer {
 
@@ -16,43 +14,80 @@ public class ShapeRecognizer {
     }
 
     public static void main(String[] args) {
-
-        Mat shapeTemplate;
+        String shapeOnCultivatedImage;
         Mat shapeCultivated;
-
-        shapeTemplate = Imgcodecs.imread("./shapes/");
-        shapeCultivated = Imgcodecs.imread("./shapes/");
-
-        Mat shapeGrayTemplate = new Mat();
+        shapeCultivated = Imgcodecs.imread("./shapes/circle.jpg");
         Mat shapeGrayCultivated = new Mat();
-        Mat shapeDstTemplate = new Mat();
         Mat shapeDstCultivated = new Mat();
-
-        Imgproc.cvtColor(shapeTemplate, shapeGrayTemplate, COLOR_RGB2GRAY);
         Imgproc.cvtColor(shapeCultivated, shapeGrayCultivated, COLOR_RGB2GRAY);
-        Imgproc.Canny(shapeGrayTemplate, shapeDstTemplate, 100, 600);
-        Imgproc.Canny(shapeGrayCultivated, shapeDstCultivated, 100, 600);
-        //Imgcodecs.imwrite("GRAYcultivated.jpg", shapeGrayCultivated);
-        //Imgcodecs.imwrite("DSTcultivated.jpg", shapeDstCultivated);
+        Imgproc.Canny(shapeGrayCultivated, shapeDstCultivated, 50, 500);
+        List<MatOfPoint> shapeContoursCultivated = new ArrayList<>();
+        Imgproc.findContours(shapeDstCultivated, shapeContoursCultivated, new Mat(), Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
 
-        List<MatOfPoint> ShapeContoursTemplate = new ArrayList<>();
-        List<MatOfPoint> ShapeContoursCultivated = new ArrayList<>();
+//        Imgcodecs.imwrite("GRAYcultivated.jpg", shapeGrayCultivated);
+//        Imgcodecs.imwrite("DSTcultivated.jpg", shapeDstCultivated);
 
-        Imgproc.findContours(shapeDstTemplate, ShapeContoursTemplate, new Mat(), Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
-        Imgproc.findContours(shapeDstCultivated, ShapeContoursCultivated, new Mat(), Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
-
-
-        double matchValue = 0;
-        for (MatOfPoint matOfPoint : ShapeContoursTemplate) {
-            Mat image1 = matOfPoint.t();
-            for (MatOfPoint matOfPoint2 : ShapeContoursCultivated) {
-                Mat image2 = matOfPoint2.t();
-                matchValue += Imgproc.matchShapes(image1, image2, Imgproc.CV_CONTOURS_MATCH_I3, 0);
+        double max_area = -1;
+        int index = 0;
+        for (int i = 0; i < shapeContoursCultivated.size(); i++) {
+            if (Imgproc.contourArea(shapeContoursCultivated.get(i)) > max_area) {
+                max_area = Imgproc.contourArea(shapeContoursCultivated.get(i));
+                index = i;
             }
         }
 
-        System.out.println("Contours in template: " + ShapeContoursTemplate.size());
-        System.out.println("Contours in cultivated: " + ShapeContoursCultivated.size());
-        System.out.println("\nSimilarity: " + matchValue);
+        MatOfPoint2f approxCurve = new MatOfPoint2f();
+        MatOfPoint2f oriCurve = new MatOfPoint2f(shapeContoursCultivated.get(index).toArray());
+        Imgproc.approxPolyDP(oriCurve, approxCurve, 6.0, true);
+
+        Point[] array = approxCurve.toArray();
+//        for (int i = 0; i < array.length; i++) {
+//            System.out.println(array[i]);
+//        }
+//        System.out.println(array.length);
+
+        if (array.length == 4) {
+            double aspectRatio = (array[1].x - array[0].x) / (array[3].y - array[0].y);
+            System.out.println(aspectRatio);
+            if ((aspectRatio >= 0.95) && (aspectRatio <= 1.05)) {
+                shapeOnCultivatedImage = "square";
+            } else if (aspectRatio < -10) {
+                shapeOnCultivatedImage = "trapeze";
+            } else if (((array[1].x - array[0].x < 5) && (array[1].x - array[0].x >= 0)) || ((array[2].x - array[1].x < 5)) && (array[2].x - array[1].x >= 0)) {
+                shapeOnCultivatedImage = "rectangle";
+            } else {
+                shapeOnCultivatedImage = "rhombus";
+            }
+
+        } else if (array.length == 3) {
+            shapeOnCultivatedImage = "triangle";
+        } else {
+            double maxX = 0;
+            double minX = 10000;
+            double maxY = 0;
+            double minY = 10000;
+            double average;
+            for (Point anArray : array) {
+                if (anArray.x > maxX) {
+                    maxX = anArray.x;
+                }
+                if (anArray.x < minX) {
+                    minX = anArray.x;
+                }
+                if (anArray.y > maxY) {
+                    maxY = anArray.y;
+                }
+                if (anArray.y < minY) {
+                    minY = anArray.y;
+                }
+            }
+            average = ((maxX - minX) - (maxY - minY));
+            if (average == 0) {
+                shapeOnCultivatedImage = "circle";
+            } else {
+                shapeOnCultivatedImage = "ellipse";
+            }
+        }
+        System.out.println(shapeOnCultivatedImage);
     }
 }
